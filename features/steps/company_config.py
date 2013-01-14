badd67bdd721a71723a511d3bdea7b645cbc0eba
unknown <fclementi@camptocamp.com>
2013-01-14 17:57:30 +0100

    [FIX] DSL about company currency

--- features/steps/company_config.py
+++ features/steps/company_config.py
@@ -24,6 +24,7 @@ def impl(ctx, rate_code, rate_value):
     rate = model('res.currency.rate').get([('currency_id', '=', currency_id)])
     assert rate
     rate.write({'rate': rate_value})
+    company.write({'currency_id': rate.id})
 
 @given(u'I set the webkit path to "{webkit_path}"')
 def impl(ctx, webkit_path):
