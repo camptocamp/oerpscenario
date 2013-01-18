5f344023eabd35920f969c41efe91ec2d49b8e5e
unknown <nicolas.bessi@camptocamp.com>
2013-01-18 17:18:07 +0100

    [FIX] bank

--- features/steps/bank_config.py
+++ features/steps/bank_config.py
@@ -1,9 +1,13 @@
 from support import model, assert_equal, set_trace, puts
 
-@given(u'there is a journal with {field} "{value}" from company "{company_name}"')
+@given(u'there is a journal with {field} "{value}" and company "{company_name}"')
 def impl(ctx, field, value, company_name):
     company = model('res.company').get([('name', '=', company_name)])
     assert company
     journal = model('account.journal').get([(field, '=', value), ('company_id', '=', company.id)])
+    # if not journal:
+    #     import pdb; pdb.set_trace()
+
     assert journal
     ctx.found_item = journal
+    ctx.search_model_name = 'account.journal'
