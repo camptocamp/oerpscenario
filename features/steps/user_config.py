91fdb34ca820fe0bdb969434d2e2cd01345d8040
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2013-01-25 11:18:48 +0100

    [ADD] search by full_name for security groups to assign to users

--- features/steps/user_config.py
+++ features/steps/user_config.py
@@ -27,10 +27,19 @@ def impl(ctx):
 
 @given(u'we assign to {users} the groups bellow')
 def impl(ctx, users):
-    # FIXME name is not unique we have to use computed name
+    # search groups by name and full name
     group_names = [row['group_name'] for row in ctx.table]
     group_names = list(set(group_names))
-    groups = model('res.groups').browse([('name', 'in', group_names)])
+    group_full_names = [name for name in group_names if '/' in name]
+    group_names = [name for name in group_names if not '/' in name]
+    search_cond = [('name', 'in', group_names)]
+    # it isn't possible to use the 'in' operator on full_name
+    full_name_conds = [('full_name', '=', full_name)
+            for full_name in group_full_names]
+    for cond in full_name_conds:
+        search_cond.insert(0, '|')
+        search_cond.append(cond)
+    groups = model('res.groups').browse(search_cond)
     # assert_equal(len(groups), len(group_names))
     assert users in ('user', 'users')
     if users == "users":
