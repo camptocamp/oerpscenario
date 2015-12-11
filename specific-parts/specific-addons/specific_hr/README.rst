8673686cd7aea72f181da7b93331c4d62a000446
Laurent Meuwly <laurent.meuwly@camptocamp.com>
2015-12-11 23:14:08 +0100

    [Add] Addons to extend hr. Actuallay by adding substitute to manager on department.

---
+++ specific-parts/specific-addons/specific_hr/README.rst
@@ -0,0 +1,11 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+================
+HR
+================
+
+This module extends some features on hr:
+
+- add a substitute person to manager on department
---
+++ specific-parts/specific-addons/specific_hr/models/hr_department.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# © 2015 Laurent Meuwly (Camptocamp)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import models, fields
+
+class HrDepartment(models.Model):
+    _inherit = 'hr.department'
+
+    substitute_id = fields.Many2one(
+        comodel_name='hr.employee',
+        string='Substitute',
+        track_visibility='onchange'
+    )
