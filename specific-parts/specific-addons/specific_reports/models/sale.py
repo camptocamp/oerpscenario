56e91bdd871fb7d80ed7edd8220c567ad085e1d8
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-02-03 16:48:46 +0100

    Quotation report

---
+++ specific-parts/specific-addons/specific_reports/models/sale.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# © 2016 Yannick Vaucher (Camptocamp)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    client_order_descr = fields.Char(
+        "Client order description",
+        help="Description of how the order was made"
+    )
+    delivery_term = fields.Date(
+        "Term of delivery",
+    )
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    price_unit_discount = fields.Monetary(
+        compute='_compute_price_discount', string='Subtotal',
+        readonly=True
+    )
+
+    @api.one
+    @api.depends('price_unit', 'discount')
+    def _compute_price_discount(self):
+        if self.discount:
+            if self.discount == 100:
+                discount = 0.0
+            else:
+                discount = 1 - self.discount / 100.0
+            self.price_unit_discount = self.price_unit * discount
+        else:
+            self.price_unit_discount = self.price_unit
