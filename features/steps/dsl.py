d17cffd12e6dc3a5e720920a66e0669fba153641
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-02-04 16:22:30 +0100

    Adapt option settings for 9.0. Allowing to use base menu name, full path name or xml id

--- features/steps/dsl.py
+++ features/steps/dsl.py
@@ -138,60 +138,36 @@ def impl(ctx, company_oid):
     company = model('res.company').get(c_domain)
     ctx.company_id = company.id
 
-
-@step(u'I set "{option}" to "{value}" in "{menu_xml_id}" settings menu by xml_id')
-def set_in_settings(ctx, option, value, menu_xml_id):
-    """ Define value of an option in settings by xml_id """
-    Menu = model('ir.ui.menu')
-    Field = model('ir.model.fields')
-    module, xmlid = menu_xml_id.split('.', 1)
-    _model, id = model('ir.model.data').get_object_reference(module, xmlid)
-    settings_menu = Menu.browse(id)
-    assert settings_menu, "menu %s was not found" % menu_xml_id
-    wiz_model = settings_menu.action.res_model
-
-    field = Field.get([('model', '=', wiz_model),
-                       ('field_description', '=', option)])
-    assert field
-    Wiz = model(wiz_model)
-    values = {}
-    if wiz_model == 'account.config.settings':
-        # Special call to onchange in case of account config
-        company = model('res.users').browse(1).company_id
-        values.update(Wiz.onchange_company_id(None, company.id)['value'])
-    field_model = model(field.model)
-
-    if field_model._fields[field.name]['type'] == 'selection':
-        selection = field_model._fields[field.name]['selection']
-        for elem in selection:
-            if elem[1] == value:
-                values[field.name] = elem[0]
-    else:
-        values[field.name] = value
-    config = Wiz.create(values)
-
-    config.execute()
-
-
 @step(u'I set "{option}" to "{value}" in "{menu}" settings menu')
 def set_in_settings(ctx, option, value, menu):
-    """ Define value of an option in settings by name """
+    """ Define value of an option in settings by name
+    :param menu: 8.0: Menu name or xmlid
+                 9.0: base menu name or full path or xmlid
+    """
     Menu = model('ir.ui.menu')
     Field = model('ir.model.fields')
-    search_domain = [('module', '=', 'base'), ('name', '=', 'menu_config')]
-    base_menu_config_id = model('ir.model.data').get(search_domain).res_id
-    settings_menu = Menu.get(
-        [('name', '=', menu),
-         ('parent_id', '=', base_menu_config_id)])
-    if not settings_menu and menu in ('Invoicing', 'Accounting'):
-        # Try with alias name (depending on module installed)
-        if menu == 'Invoicing':
-            alias = 'Accounting'
-        elif menu == 'Accounting':
-            alias = 'Invoicing'
-        settings_menu = Menu.get(
-            [('name', '=', alias),
-             ('parent_id', '=', base_menu_config_id)])
+    if menu.startswith('oid:'):
+        settings_menu = Menu.get(menu[4:])
+    elif 'Accounting' in menu or 'Invoicing' in menu:
+        settings_menu = Menu.get('account.menu_account_config')
+    else:
+        if ctx.conf['server'].release.major_version >= 9.0:
+            # search by full path or compose full path
+            # but we need to loop on menu as full path is not stored
+            # thus not searchable
+            if not '/' in menu:
+                menu = menu + '/Configuration/Settings'
+            domain = [('name', '=', menu.split('/')[-1])]
+            menus = Menu.browse(domain)
+            for m in menus:
+                if m.complete_name == menu:
+                    settings_menu = m
+                    break
+        else:
+            base_menu_config = Menu.get('base.menu_config')
+            settings_menu = Menu.get(
+                [('name', '=', menu),
+                 ('parent_id', '=', base_menu_config.id)])
     assert settings_menu, "menu %s was not found" % menu
     wiz_model = settings_menu.action.res_model
 
@@ -205,7 +181,17 @@ def set_in_settings(ctx, option, value, menu):
         company = model('res.users').browse(1).company_id
         values.update(Wiz.onchange_company_id(None, company.id)['value'])
 
-    values[field.name] = value
+    field_model = model(field.model)
+    if field_model._fields[field.name]['type'] == 'selection':
+        selection = field_model._fields[field.name]['selection']
+        for elem in selection:
+            if elem[1] == value:
+                values[field.name] = elem[0]
+                break
+        assert values.get(field.name) is not False, "Value not found in selection"
+    else:
+        values[field.name] = value
+    # This might fail du to https://github.com/odoo/odoo/issues/10775
     config = Wiz.create(values)
 
     config.execute()
