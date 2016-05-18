8d481e7c51261f854adf03b9ced83a5e812cec06
Denis Leemann <denis.leemann@camptocamp.com>
2016-05-18 14:51:35 +0200

    flake8 & manual code

--- specific-parts/specific-addons/specific_reports/models/stock.py
+++ specific-parts/specific-addons/specific_reports/models/stock.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # © 2016 Yannick Vaucher (Camptocamp)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import api, fields, models
+from openerp import api, models
 
 
 class StockPicking(models.Model):
@@ -9,7 +9,7 @@ class StockPicking(models.Model):
 
     @api.multi
     def get_employee_from_user(self, user_id=None):
-        so = self.env['sale.order'].search([('name','=', self.origin)])
+        so = self.env['sale.order'].search([('name', '=', self.origin)])
         if not user_id:
             hr_employee = so.get_employee_from_user(so.user_id)
         else:
