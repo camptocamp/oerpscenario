f97272c1218c0830601f2e71796770ab4856e76f
Cyril Gaudin <cyril.gaudin@camptocamp.com>
2016-09-21 11:51:27 +0200

    fix delivery slip when not user_id.

--- specific-parts/specific-addons/specific_reports/models/stock.py
+++ specific-parts/specific-addons/specific_reports/models/stock.py
@@ -12,10 +12,12 @@ class StockPicking(models.Model):
         so = self.env['sale.order'].search([('name', '=', self.origin)])
         if not so and not user_id:
             user_id = self.partner_id.user_id
+        elif not user_id:
+            user_id = so.user_id
 
-        if not user_id:
-            hr_employee = so.get_employee_from_user(so.user_id)
-        else:
+        if user_id:
             hr_employee = so.get_employee_from_user(user_id)
+        else:
+            hr_employee = False
 
         return hr_employee
