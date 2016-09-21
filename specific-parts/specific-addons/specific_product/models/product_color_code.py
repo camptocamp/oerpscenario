00532d687533aade7929c168a4227a94d19756ad
Cyril Gaudin <cyril.gaudin@camptocamp.com>
2016-09-21 11:02:12 +0200

    Product color: Display code in color name.

--- specific-parts/specific-addons/specific_product/models/product_color_code.py
+++ specific-parts/specific-addons/specific_product/models/product_color_code.py
@@ -2,7 +2,7 @@
 # © 2016 Cyril Gaudin (Camptocamp)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from openerp import api, fields, models
 
 
 class ProductColorCode(models.Model):
@@ -11,3 +11,21 @@ class ProductColorCode(models.Model):
     name = fields.Char(string="Color Name", required=True)
     code = fields.Char("NCS/RAL Code", required=True)
     feller_name = fields.Char("Color Name Feller")
+
+    @api.multi
+    def name_get(self):
+        return [
+            (record.id, "[%s] %s" % (record.code, record.name))
+            for record in self
+        ]
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        args = args or []
+        domain = []
+        if name:
+            domain = [
+                '|', ('name', operator, name), ('code', operator, name)
+            ]
+        colors = self.search(domain + args, limit=limit)
+        return colors.name_get()
